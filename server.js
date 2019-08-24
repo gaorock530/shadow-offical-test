9a2049d2fd656edd89c45d0cec5c9659efcfc18d
gaorock530 <gaorock530@hotmail.com>
2019-08-25 00:59:46 +0800

    add ssl

--- server.js
+++ server.js
@@ -1,10 +1,21 @@
+const https = require('https');
+const fs = require('fs');
+const path = require('path');
+
 const express = require('express');
 const app = express();
 
 
 app.use(express.static(__dirname + '/build'));
 
+const options = {
+  key: fs.readFileSync(path.join(__dirname, '/ssl/www/2_www.yingxitech.com.key')),
+  cert: fs.readFileSync(path.join(__dirname, '/ssl/www/1_www.yingxitech.com_bundle.crt'))
+};
 
-app.listen(8888, (e) => {
+const server = https.createServer(options, app);
+
+
+server.listen(8888, (e) => {
   console.log(e || 'Server is running on port 8888.');
-})
+});
