cb80e15594c87e7f73d2091c0f029d9b29a4833a
gaorock530 <gaorock530@hotmail.com>
2019-09-18 15:05:44 +0800

    update server

--- server.js
+++ server.js
@@ -8,6 +8,7 @@ const app = express();
 app.set('x-powered-by', false);
 app.use(express.static(__dirname + '/build'));
 
+const PORT = 3000;
 
 const options = {
   key: fs.readFileSync(path.join(__dirname, '/ssl/www/2_www.yingxitech.com.key')),
@@ -17,6 +18,6 @@ const options = {
 const server = https.createServer(options, app);
 
 
-server.listen(8888, (e) => {
-  console.log(e || 'Server is running on port 8888.');
+server.listen(PORT, (e) => {
+  console.log(e || `Server is running on port ${PORT}.`);
 });
